f855f0c7ddb7d327550b28dad335965052ebc13d
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2024-07-14 14:52:25 +0100

    initial commit

---
+++ BlockBattles/src/main/java/net/tiagofar78/blockbattles/BlockBattles.java
@@ -0,0 +1,12 @@
+package net.tiagofar78.blockbattles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockBattles extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+
+}
